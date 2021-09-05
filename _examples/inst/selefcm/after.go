569b62285d2060ced05003588e33c0d737dea88b
Shihao Xia <charlesxsh@hotmail.com>
2021-09-05 17:45:56 -0400

    update selefcm example

--- _examples/inst/selefcm/after.go
+++ _examples/inst/selefcm/after.go
@@ -2,13 +2,102 @@ package selefcm
 
 import (
 	gooracle "gooracle"
+	"sync"
 	"time"
 )
 
-func Hello() {
+func SelectWithCh() {
 	ch1 := make(chan int)
 	ch2 := make(chan struct{})
-	switch gooracle.ReadSelect("/gfuzz/_examples/inst/selefcm/before.go", 9, 3) {
+	switch gooracle.ReadSelect("/Users/xsh/code/GFuzz/_examples/inst/selefcm/before.go", 12, 2) {
+	case 0:
+		select {
+		case <-ch1:
+			println("ch1!")
+		case <-gooracle.SelectTimeout():
+			gooracle.StoreLastMySwitchChoice(-1)
+			select {
+			case <-ch1:
+				println("ch1!")
+			case <-ch2:
+				println("ch2")
+			}
+		}
+	case 1:
+		select {
+		case <-ch2:
+			println("ch2")
+		case <-gooracle.SelectTimeout():
+			gooracle.StoreLastMySwitchChoice(-1)
+			select {
+			case <-ch1:
+				println("ch1!")
+			case <-ch2:
+				println("ch2")
+			}
+		}
+	default:
+		gooracle.StoreLastMySwitchChoice(-1)
+		select {
+		case <-ch1:
+			println("ch1!")
+		case <-ch2:
+			println("ch2")
+		}
+	}
+}
+
+func SelectWithDefault() {
+	ch1 := make(chan int)
+	ch2 := make(chan struct{})
+	switch gooracle.ReadSelect("/Users/xsh/code/GFuzz/_examples/inst/selefcm/before.go", 24, 3) {
+	case 0:
+		select {
+		case <-ch1:
+			println("ch1!")
+		case <-gooracle.SelectTimeout():
+			gooracle.StoreLastMySwitchChoice(-1)
+			select {
+			case <-ch1:
+				println("ch1!")
+			case <-ch2:
+				println("ch2")
+			default:
+				print("default")
+			}
+		}
+	case 1:
+		select {
+		case <-ch2:
+			println("ch2")
+		case <-gooracle.SelectTimeout():
+			gooracle.StoreLastMySwitchChoice(-1)
+			select {
+			case <-ch1:
+				println("ch1!")
+			case <-ch2:
+				println("ch2")
+			default:
+				print("default")
+			}
+		}
+	default:
+		gooracle.StoreLastMySwitchChoice(-1)
+		select {
+		case <-ch1:
+			println("ch1!")
+		case <-ch2:
+			println("ch2")
+		default:
+			print("default")
+		}
+	}
+}
+
+func SelectWithTimeout() {
+	ch1 := make(chan int)
+	ch2 := make(chan struct{})
+	switch gooracle.ReadSelect("/Users/xsh/code/GFuzz/_examples/inst/selefcm/before.go", 38, 3) {
 	case 0:
 		select {
 		case <-ch1:
@@ -24,7 +113,10 @@ func Hello() {
 				println("timeout")
 			}
 		}
-	case 1:
+	case
+
+		// Example from Paper "Who Goes First? Detecting Go Concurrency Bugs via Message Reordering" ====
+		1:
 		select {
 		case <-ch2:
 			println("ch2")
@@ -65,4 +157,64 @@ func Hello() {
 			println("timeout")
 		}
 	}
+}
+
+type keeper struct {
+	ch              chan int
+	deleteTokenFunc func(*token)
+}
+type token struct {
+	k  *keeper
+	mu sync.RWMutex
+}
+
+func (t *token) assignTokenToUser() { //Goroutine1
+	t.mu.Lock()
+	t.k.ch <- 0
+	t.mu.Unlock()
+}
+func (k *keeper) run() { //Goroutine2
+	ticker := time.NewTicker()
+	switch gooracle.ReadSelect("/Users/xsh/code/GFuzz/_examples/inst/selefcm/before.go", 65, 2) {
+	case 0:
+		select
+
+		// t is created before
+		{
+		case <-k.ch:
+			println("hey, I got selected")
+		case <-gooracle.SelectTimeout():
+			gooracle.StoreLastMySwitchChoice(-1)
+			select {
+			case <-k.ch:
+				println("hey, I got selected")
+			case <-ticker:
+				k.deleteTokenFunc(t)
+			}
+		}
+	case 1:
+		select {
+		case <-ticker:
+			k.deleteTokenFunc(t)
+		case <-gooracle.SelectTimeout():
+			gooracle.StoreLastMySwitchChoice(-1)
+			select {
+			case <-k.ch:
+				println("hey, I got selected")
+			case <-ticker:
+				k.deleteTokenFunc(t)
+			}
+		}
+	default:
+		gooracle.StoreLastMySwitchChoice(-1)
+		select {
+		case <-k.ch:
+			println("hey, I got selected")
+		case <-ticker:
+			k.deleteTokenFunc(t)
+		}
+	}
+}
+func newDeleteFunc(t *token) {
+	t.mu.Lock()
 }
--- _examples/inst/selefcm/before.go
+++ _examples/inst/selefcm/before.go
@@ -1,8 +1,37 @@
 package selefcm
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
-func Hello() {
+func SelectWithCh() {
+	ch1 := make(chan int)
+	ch2 := make(chan struct{})
+
+	select {
+	case <-ch1:
+		println("ch1!")
+	case <-ch2:
+		println("ch2")
+	}
+}
+
+func SelectWithDefault() {
+	ch1 := make(chan int)
+	ch2 := make(chan struct{})
+
+	select {
+	case <-ch1:
+		println("ch1!")
+	case <-ch2:
+		println("ch2")
+	default:
+		print("default")
+	}
+}
+
+func SelectWithTimeout() {
 	ch1 := make(chan int)
 	ch2 := make(chan struct{})
 
@@ -14,4 +43,32 @@ func Hello() {
 	case <-time.After(10):
 		println("timeout")
 	}
+}
+
+// Example from Paper "Who Goes First? Detecting Go Concurrency Bugs via Message Reordering" ====
+type keeper struct {
+	ch              chan int
+	deleteTokenFunc func(*token)
+}
+type token struct {
+	k  *keeper
+	mu sync.RWMutex
+}
+
+func (t *token) assignTokenToUser() { //Goroutine1
+	t.mu.Lock()
+	t.k.ch <- 0
+	t.mu.Unlock()
+}
+func (k *keeper) run() { //Goroutine2
+	ticker := time.NewTicker()
+	select {
+	case <-k.ch:
+		println("hey, I got selected")
+	case <-ticker:
+		k.deleteTokenFunc(t) // t is created before
+	}
+}
+func newDeleteFunc(t *token) {
+	t.mu.Lock()
 }
