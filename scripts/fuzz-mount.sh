7c4b74a55d02ce4dd8e1f69c4cf68e0d2fe05596
Yu Liang <luy70@psu.edu>
2021-11-19 14:23:18 -0500

    Fix a small typo in the fuzz-mount.sh script.

--- scripts/fuzz-mount.sh
+++ scripts/fuzz-mount.sh
@@ -9,6 +9,6 @@ docker build -f docker/fuzzer/Dockerfile -t gfuzz:latest .
 
 docker run --rm -it \
 -v $GOMOD_DIR:/fuzz/target \
--v $OU_DIR:/fuzz/output \
+-v $OUT_DIR:/fuzz/output \
 -v $(pwd)/tmp/pkgmod:/go/pkg/mod \
 gfuzz:latest true /fuzz/target /fuzz/output $@
