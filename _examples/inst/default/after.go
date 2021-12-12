2910f894e5d3d49bf1f60bc0217ca51232a7881d
Linhai Song <songlinhai0543@gmail.com>
2021-12-12 16:46:27 -0500

    Merge pull request #12 from system-pclub/shihaox/inst-type
    fix instrumentation issues

---
+++ _examples/inst/default/after.go
@@ -0,0 +1,51 @@
+package defaultp
+
+import (
+	oraclert "gfuzz/pkg/oraclert"
+	aaa "sync"
+)
+
+type aa struct {
+	m aaa.Mutex
+}
+
+func (_ *aa) abcde() {
+	println(3)
+}
+
+func (a *aa) abcd() {
+	oraclert.CurrentGoAddValue(a, nil, 0)
+	b := 2
+	println(a, b)
+}
+func Hello() {
+	m := aaa.Mutex{}
+
+	c := aaa.NewCond(&m)
+	oraclert.StoreOpInfo("Broadcast",
+
+		//asdfasdf
+		1)
+
+	c.Broadcast()
+	oraclert.StoreOpInfo("Signal", 2)
+
+	c.Signal()
+	oraclert.StoreOpInfo("Wait",
+
+		//asdfadfas
+		3)
+
+	c.Wait()
+
+	w := aaa.WaitGroup{}
+	oraclert.StoreOpInfo("Wait", 5)
+
+	w.Wait()
+
+	a := aa{}
+	oraclert.StoreOpInfo("Lock", 4)
+
+	a.m.Lock()
+
+}
--- scripts/gen-examples.sh
+++ scripts/gen-examples.sh
@@ -4,3 +4,4 @@
 ./bin/inst --file _examples/inst/selefcm/before.go --out _examples/inst/selefcm/after.go --pass selefcm --check-syntax-err
 ./bin/inst --file _examples/inst/wgrec/before.go --out _examples/inst/wgrec/after.go --pass wgrec --check-syntax-err
 ./bin/inst --file _examples/inst/oracle/before.go --out _examples/inst/oracle/after.go --pass oracle --check-syntax-err
+./bin/inst --file _examples/inst/default/before.go --out _examples/inst/default/after.go --check-syntax-err
