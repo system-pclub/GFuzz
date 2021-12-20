3eb17b2ae223a6217109980586aa48ad5e7708e8
Yu Liang <luy70@psu.edu>
2021-12-19 19:05:22 -0500

    Fix a problem that cause score accumulated adding.

--- pkg/fuzz/score/impl.go
+++ pkg/fuzz/score/impl.go
@@ -7,10 +7,10 @@ import (
 
 const (
 	ScoreTupleCountLog2 = 1
-	ScoreCh = 10
-	ScoreNewClosed = 10
-	ScoreNewNotClosed = 10
-	ScoreBuf = 10
+	ScoreCh             = 10
+	ScoreNewClosed      = 10
+	ScoreNewNotClosed   = 10
+	ScoreBuf            = 10
 )
 
 type ScoreStrategyImpl struct {
@@ -26,37 +26,43 @@ func NewScoreStrategyImpl(fctx *api.Context) api.ScoreStrategy {
 func (s *ScoreStrategyImpl) Score(i *api.Input, o *api.Output) (int, error) {
 	var totalScore int = 0
 	var tupleScore int = 0
-	var chNotclosedScore int = 0
-	//var ch_closed_score int = 0
+	//var chNotclosedScore int = 0
+	var chClosedScore int = 0
 	var bufferScore int = 0
 	var channelScore int = 0
 
 	/* Calculate tuple score */
 	for _, count := range o.OracleRtOutput.Tuples {
 		tupleScore += int(math.Log2(float64(count))) * ScoreTupleCountLog2
-		totalScore += tupleScore
 	}
 
 	for _, chRecord := range o.OracleRtOutput.Channels {
 
 		/* Calculate score for first time closed or not closed channel */
-		if chRecord.NotClosed {
-			chNotclosedScore += ScoreNewNotClosed
-			totalScore += chNotclosedScore
+		//if chRecord.NotClosed {
+		//	chNotclosedScore += ScoreNewNotClosed
+		//}
+
+		if chRecord.Closed {
+			chClosedScore += ScoreNewClosed
 		}
+
 		//TODO:: Missing first time closed?
 
 		/* Calculate score for score buffer */
-		if 	chRecord.PeakBuf > 0 && chRecord.CapBuf != 0 {
+		if chRecord.PeakBuf > 0 && chRecord.CapBuf != 0 {
 			bufferPer := float64(chRecord.PeakBuf) / float64(chRecord.CapBuf)
 			bufferScore += int(float64(ScoreBuf) * bufferPer)
-			totalScore += bufferScore
 		}
 
 		/* Calculate score for each encountered channel */
 		channelScore += ScoreCh
-		totalScore += ScoreCh
 	}
+
+	totalScore += tupleScore
+	totalScore += chClosedScore
+	totalScore += bufferScore
+	totalScore += channelScore
 
 	return totalScore, nil
 }
